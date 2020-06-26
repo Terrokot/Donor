aa59cf60bfc52d22be1a2c54204dd6189f2445ae
TerroKot <egor.tereshonok@gmail.com>
2020-06-26 17:12:06 +0300

    update log screen

--- Donor/ViewController.swift
+++ Donor/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginTextField: UIButton!
+    
+    @IBOutlet weak var donorLabel: UILabel!
+    @IBOutlet weak var patientLabel: UILabel!
+    @IBOutlet weak var donorPatientSwitch: UISwitch!
+    
+    @IBOutlet weak var topButton: UIButton!
+    @IBOutlet weak var bottomButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func topTapped(_ sender: Any) {
+    }
+    
+    @IBAction func bottomTapped(_ sender: Any) {
+    }
 }
 
