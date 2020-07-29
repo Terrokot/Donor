1fe2268ae543557b51b55c00e55b969131d23064
TerroKot <egor.tereshonok@gmail.com>
2020-07-30 02:41:32 +0300

    design update

--- Donor/UI/Custom UI Elements/AuthTextField.swift
+++ Donor/UI/Custom UI Elements/AuthTextField.swift
@@ -10,58 +10,31 @@ import UIKit
 
 class AuthTextField: UITextField {
     
-    var containerView = UIView()
-    
-    var mainView: UIView = {
-        let view = UIView()
-        view.backgroundColor = AuthColors.backgroundTextField
-        view.layer.shadowRadius = 5
-        view.layer.shadowOffset = CGSize(width: 3, height: 3)
-        view.layer.shadowOpacity = 0.5
-        view.layer.cornerRadius = 20
-        view.layer.borderWidth = 2
-        view.layer.borderColor = AuthColors.borderColor.cgColor
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
-    }()
-    
-    var contentsLayer: UIView = {
-        let view = UIView()
-        view.backgroundColor = AuthColors.backgroundTextField
-        view.layer.cornerRadius = 25
-        view.layer.masksToBounds = true
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
-    }()
-    
-    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        setup()
+        setupButton()
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        setup()
+        setupButton()
     }
     
-    func setup() {
-        backgroundColor = .white
-        addSubview(mainView)
-        mainView.addSubview(contentsLayer)
-        
-        NSLayoutConstraint.activate([
-            // Constrains your mainView to the ViewController's view
-            mainView.centerYAnchor.constraint(equalTo: centerYAnchor),
-            mainView.centerXAnchor.constraint(equalTo: centerXAnchor),
-            mainView.heightAnchor.constraint(equalToConstant: frame.height),
-            mainView.widthAnchor.constraint(equalToConstant: frame.width),
-            // Constrains your contentsLayer to the mainView
-            contentsLayer.centerYAnchor.constraint(equalTo: mainView.centerYAnchor),
-            contentsLayer.centerXAnchor.constraint(equalTo: mainView.centerXAnchor),
-            contentsLayer.heightAnchor.constraint(equalTo: mainView.heightAnchor),
-            contentsLayer.widthAnchor.constraint(equalTo: mainView.widthAnchor)
-        ])
-        
+    func setupButton() {
+        setShadow()
+        backgroundColor      = AuthColors.backgroundTextField
+        layer.cornerRadius   = frame.height / 2
+        layer.borderWidth    = 2
+        layer.borderColor    = AuthColors.borderColor.cgColor
+    }
+    
+    
+    private func setShadow() {
+        layer.shadowColor   = UIColor.black.cgColor
+        layer.shadowOffset  = CGSize(width: 0.0, height: 6.0)
+        layer.shadowRadius  = 5
+        layer.shadowOpacity = 0.5
+        clipsToBounds       = true
+        layer.masksToBounds = false
     }
 }
