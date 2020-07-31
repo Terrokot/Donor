8d7e443489b9a07f927ad26249b8ff41a9a623b8
TerroKot <egor.tereshonok@gmail.com>
2020-07-31 20:04:46 +0300

    add comments

--- Donor/UI/Authorization/AuthExtensions.swift
+++ Donor/UI/Authorization/AuthExtensions.swift
@@ -7,3 +7,60 @@
 //
 
 import Foundation
+import UIKit
+
+//MARK: UI Setup
+extension AuthViewController {
+    
+    func setupUI() {
+        segmentedControllerSetup()
+        installConstraints()
+    }
+    
+    private func  segmentedControllerSetup() {
+        //setup custom SegC
+        donorPatientSegmentedController.items = ["Donor", "Patient"]
+        donorPatientSegmentedController.font = UIFont(name: "Avenir-Black", size: 20)
+        donorPatientSegmentedController.selectedLabelColor = .white
+        donorPatientSegmentedController.borderColor = UIColor(red: 0.45, green: 0.49, blue: 0.55, alpha: 1.00)
+        donorPatientSegmentedController.unselectedLabelColor = .lightGray
+        donorPatientSegmentedController.thumbColor = AuthColors.red
+        donorPatientSegmentedController.backgroundColor = .white
+        donorPatientSegmentedController.selectedIndex = 0
+        donorPatientSegmentedController.padding = 0
+    }
+    
+    private func installConstraints() {
+        
+        switch view.frame.height {
+        case let x where x > 667:
+            // lager than iphone 8+
+            logoTopConstraint.constant         = view.frame.height / 11
+            authStackTopConstraint.constant    = view.frame.height / 10.8
+            AuthSwitchStack.spacing            = view.frame.height / 16.9
+            AuthStack.spacing                  = view.frame.height / 17.1
+            loginTopConstraint.constant        = view.frame.height / 17.1
+            statusLabelTopConstraint.constant  = view.frame.height / 10.3
+        case let x where x < 667:
+            // smaller than iphone 8
+            logoTopConstraint.constant         = view.frame.height / 14
+            authStackTopConstraint.constant    = view.frame.height / 13
+            AuthSwitchStack.spacing            = view.frame.height / 20
+            AuthStack.spacing                  = view.frame.height / 22
+            loginTopConstraint.constant        = view.frame.height / 22
+            statusLabelTopConstraint.constant  = view.frame.height / 13
+        default:
+            break
+        }
+    }
+}
+
+//MARK: UITextFieldDelegate
+extension AuthViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }
+}
+
